f1102b0bca4761dbafa3297474d862efc938e149
Alexandre Day <alexandre.day1@gmail.com>
2017-10-26 08:01:36 -0400

    mod gitignore

--- check_sol.py
+++ check_sol.py
@@ -3,4 +3,8 @@ from kSAT import KSAT
 
 # -----------> just check solutions --->
 model = KSAT()
-model.check_all_solution('sol_N=1000_M=3500_alpha=3.50_K=3.txt','formula.tmp_N=1000_M=3500_alpha=3.50_K=3.cnf')
+M=4050
+a = 4.05
+sfile = 'sol_N=1000_M=%i_alpha=%.2f_K=3.txt'%(M,a)
+ffile = 'formula.tmp_N=1000_M=%i_alpha=%a_K=3.cnf'%(M,a)
+model.check_all_solution(sfile,ffile)
--- solutions/tsne_this.py
+++
@@ -1,9 +0,0 @@
-from tsne_visual import TSNE
-from matplotlib import pyplot as plt
-import numpy as np
-
-X=np.loadtxt('sol_N1000=_M=3900_alpha=3.90_K=3.txt',dtype=float)
-Xtsne = TSNE(perplexity=50.0, n_iter=1000, angle=0.3).fit_transform(X)
-np.savetxt('tsne1.txt', Xtsne)
-plt.scatter(Xtsne[:,0], Xtsne[:,1])
-plt.show()
