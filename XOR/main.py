904f67010ef4f48d04ba42b2e93df85f57d7650d
alexandreday <alexandre.day1@gmail.com>
2018-03-18 15:28:58 -0400

    update draft

--- XOR/main.py
+++ XOR/main.py
@@ -3,9 +3,29 @@ from scipy.sparse import coo_matrix, csc_matrix
 
 def main():
     np.random.seed(1)
-    y, A = construct_formula(10,0.5)
-    print(A.toarray())
+    y, A = construct_formula(10, 0.5)
 
+    print(A.toarray())
+    solve(A, y)
+
+    # ----------- > < 
+    # ----------- > <
+    exit()
+    #print(A.toarray())
+    print((True + True) % 2)
+    print(np.array([-1,0,1])%2)
+    exit()
+    tmp = A[0,:].tocoo()
+    print(tmp.data)
+    print(tmp.row)
+    print(tmp.col)
+    """ for i in tmp:
+        print(i,'\t',tmp) """
+    exit()
+    for a in A[0,:]:
+        last_element = a
+    print(last_element.to)
+    exit()
     v = A[3,:]
     print(v)
     print('\n\n\n')
@@ -21,37 +41,73 @@ def main():
     print(A)
 
 
-
-
-
 def solve(A, y):
+
     # A is the adjacency matrix
-    r,c = A.shape
+    # Gaussian elimination mod 2, with sparse matrices
+    r, c = A.shape
 
     for j in range(c):
-       element_j = A[j:,j]
-       if element_j.shape[1] == 1:
-           print("empty")
-    maxj = element_j[-1][0]
-
+        if A[j,j] == 0:
+            swap_diagonal(A, j)
         
+        maxi = find_pivot(A, j) # what if 
+        print('first pivot: ',maxi)
 
+        while maxi > j: # meaning there are non-zero elements below A[j, j]
+            #print((A[maxi,:] - A[j,:]))
+            A[maxi,:] = (A[maxi,:] - A[j,:])
+            print('data: ',A[maxi,:].data[0])
+            A[maxi,:].data = mod2_data(A[maxi,:].data)
+            print('data: ',A[maxi,:].data[0])
+            exit()
+            print(A.toarray())
+            #y[maxi].data = (y[maxi] - y[j]).data %2
+            maxi = find_pivot(A, j)
+            print('second pivot', maxi)
+            exit()
 
+    # at this point A should be an upper diagonal matrix
+    print(A)
     return
+# define sparse matrix class for bool operations ... we want row add, swaps etc.
+# do no store zeros ....
+
+class SparseBool:
+
+    def __init__(self):
+        print('etc. etc.')
+    
 
 
+
+def mod2_data(data):
+    return [list(list(np.array(data[0],dtype=np.int8) %2))]
+
+def swap_diagonal(A, j):
+    i = find_pivot(A,j)
+    swap_rows(A,i,j)
+
+def find_pivot(A, j):
+    # looks for the first non-zero in column j starting from the bottom
+    # returns the row index of that element
+    return A[j:,j].tocoo().row[-1]
+
+def swap_rows(A, i, j):
+    tmp = np.copy(A[i,:])
+    A[i,:] = A[j,:]
+    A[j,:] = tmp
 
 def construct_formula(N, alpha, p = 3): # 3-XORSAT
     M=int(alpha*N)
     y = np.random.randint(0,2,size=M)
 
     A = []
-    while nc < M:
+    while len(A) < M:
         c = np.random.randint(0,N,p)
         if len(np.unique(c)) == p:
-            
+            A.append(c)
 
-    A = np.random.randint(0,N,size=3*M).reshape(M,3)
     idx_1 = []
     idx_2 = []
     for i, c in enumerate(A):
@@ -61,7 +117,7 @@ def construct_formula(N, alpha, p = 3): # 3-XORSAT
     data = np.ones(len(idx_2))
     C = coo_matrix((data,(idx_1,idx_2)), dtype=np.int8, shape=(len(A),N))
 
-    return y, C.tocsc()
+    return y, C.tolil()
 
 if __name__ == "__main__":
     main()
