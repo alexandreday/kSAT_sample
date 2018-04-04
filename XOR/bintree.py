203843d6823bcdf648cd5c17a489e40856f4d207
alexandreday <alexandre.day1@gmail.com>
2018-04-04 14:56:38 -0400

    DPLL

---
+++ XOR/bintree.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+class TREENODE:
+
+    def __init__(self, id_ = -1, parent = None, child = None, value = 0):
+        if child is None:
+            self.child = [] # has to be list of TreeNode
+        else:
+            self.child = child
+        self.value = value
+        self.parent = parent
+        self.id_ = id_
+
+    def __repr__(self):
+        return ("Node: [%s] @ s = %.3f" % (self.id_,self.scale)) 
+
+    def is_leaf(self):
+        return len(self.child) == 0
+
+    def get_child(self, id_ = None):
+        if id_ is None:
+            return self.child
+        else:
+            for c in self.child:
+                if c.get_id() == id_:
+                    return c
+
+    def get_child_id(self):
+        if len(self.child) == 0:
+            return []
+        else:
+            return [c.id_ for c in self.child]
+
+    def get_id(self):
+        return self.id_
+
+    def add_child(self, treenode):
+        self.child.append(treenode)
+    
+    def remove_child(self, treenode):
+        self.child.remove(treenode)
+        
+    def get_rev_child(self):
+        child = self.child[:]
+        child.reverse()
+        return child 
+    
+class DPLL:
+    def __init__(self):
+        return 
--- XOR/main.py
+++ XOR/main.py
@@ -1,57 +1,133 @@
 import numpy as np
 from scipy.sparse import coo_matrix, csc_matrix
 
+def check_solution(A, y, sol):
+    nclause = A.shape[0]
+    is_solution = True
+    for i in range(nclause):
+        if np.sum(A[i, :] * sol) % 2 != y[i]:
+            is_solution = False
+            break
+    return is_solution
 
 def swap_rows(A, ri, rj):
-    tmp = A[ri, :]
+    tmp = np.copy(A[ri, :]) # careful when slicing, will return a view
     A[ri, :] = A[rj, :]
     A[rj, :] = tmp
+   
+def swap(y, i, j):
+    tmp = y[i]
+    y[i] = y[j]
+    y[j] = tmp
+
+def swap_columns(A, ci, cj):
+    tmp = np.copy(A[:, ci]) # careful when slicing, will return a view
+    A[:, ci] = A[:, cj]
+    A[:, cj] = tmp
 
 def add_to_row(A, ri, rj): # A[ri, :] <- A[ri, :] + A[rj, :]
     A[ri, :] = A[ri, :] + A[rj, :]
 
 def make_UT(A_, y_):
+    """ Transforms A_ into a upper triangular matrix (where A.x == y) is a linear system of
+    equations in base 2. 
+
+    Returns
+    -------
+    (A, y, swap_history) : tuple
+        A in the UT form
+        the equivalent y associated with the new A
+        The permutations to do on the original index (x1,x2,x3, etc.) to retrive the original basis.
+    """
+
+
+    col_swap = []
     A = np.copy(A_)
     y = np.copy(y_)
 
     nr, nc = A.shape
-
+    
     for j in range(nc):
-        #A.get_first_index_non_zero(col = j)
-        pos = np.where(A[:,j] == 1)[0]
+        if j < nr:
+            #A.get_first_index_non_zero(col = j)
+            pos = np.where(A[:,j] == 1)[0]
 
-        if len(pos) > 0: # there are non-zero elements in this column
-            if A[j,j] == 0:
-                if len(pos) > 0:
+            if len(pos) > 0: # there are non-zero elements in this column
+                if A[j,j] == 0: # diagonal element is zero -> need pivot to be 1
                     if pos[-1] > j:
                         swap_rows(A, j, pos[-1])
+                        swap(y, j, pos[-1])
                         pos = pos[:-1] 
                     else: # already UT !
                         pos = []
-            for p in pos:
-                if p != j: # making elements zero !
-                    add_to_row(A, p, j)
-                    y[p] += y[j]
-        A = np.remainder(A, 2)
-        print(A)
-        if j == 4:
-            exit()
-        #exit()
-        y = np.remainder(y, 2)
-    
-    return A, y
+                        pos_row = np.where(A[j,:] == 1)[0]
+                        if len(pos_row) > 0:
+                            swap_columns(A, j, pos_row[0])
+                            col_swap.append([j, pos_row[0]])
+                            pos = np.where(A[:,j] == 1)[0]
+                            #swap(y, j, pos_row[0])                        
+                for p in pos:
+                    if p > j: # making elements zero => and only working with lower triangle
+                        add_to_row(A, p, j)
+                        y[p] += y[j]
+            A = np.remainder(A, 2)
+            y = np.remainder(y, 2)
+
+    return A, y, col_swap
+
+def walkSAT(A, y): 
+    # simple walkSAT to find solutions => fix variables at random 
+    # and try to find a solution by satisfying one constraint at the time
+    return
+
+def check_all_solution(A, y):
+    nvar = A.shape[1]
+    nsol = 2**nvar
+    b2_array = lambda n10 : np.array(list(np.binary_repr(n10, width=nvar)), dtype=np.int)
+    sol_list = []
+    for i in range(nsol):
+        sol = b2_array(i)
+        if check_solution(A, y, sol):
+            sol_list.append(sol)
+    return sol_list
+
+def swap_back(sol, swap_history):
+    nswap = len(swap_history)
+    for i in range(nswap):
+        p1, p2 = swap_history[-(i+1)]
+        swap(sol, p1, p2)
 
 def main():
 
-    np.random.seed(0)
-    A = np.random.randint(0, 2, 40).reshape(8, 5)
-    y = np.random.randint(0,2, 8)
-    print(A)
-    print(y)
-    A, y = make_UT(A, y)
-    print(A)
-    print(y)
+    #np.random.seed(0)
+    nr = 7
+    nc = 8
 
+    A = np.random.randint(0, 2, nc*nr).reshape(nr, nc)
+
+    y = np.random.randint(0, 2, nr)
+    sol_list = check_all_solution(A, y)
+    
+    print(A)
+    print(y)   
+    print(sol_list)
+    #exit()
+    Anew, ynew, swap_history = make_UT(A, y)
+
+
+    print(Anew)
+    print(ynew)
+    sol_list_new = check_all_solution(Anew, ynew)
+    for s in sol_list_new:
+        swap_back(s, swap_history)
+
+    print(sol_list_new)
+    #print(sol)
+    exit()
+    for i in range(A.shape[0]):
+        tmp = np.sum(A[i, :] * sol) %2
+        print(tmp == y[i])
+    #print(A)
     exit()
     np.random.seed(1)
     y, A = construct_formula(10, 0.5)
@@ -141,11 +217,11 @@ def find_pivot(A, j):
     # returns the row index of that element
     return A[j:,j].tocoo().row[-1]
 
-def swap_rows(A, i, j):
+""" def swap_rows(A, i, j):
     tmp = np.copy(A[i,:])
     A[i,:] = A[j,:]
     A[j,:] = tmp
-
+ """
 def construct_formula(N, alpha, p = 3): # 3-XORSAT
     M=int(alpha*N)
     y = np.random.randint(0,2,size=M)
