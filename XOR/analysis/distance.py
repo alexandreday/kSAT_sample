3157d99152270d1e157af28c4645e67c35b8a382
alexandreday <alexandre.day1@gmail.com>
2018-04-18 14:14:44 -0400

    distance

---
+++ XOR/analysis/distance.py
@@ -0,0 +1,35 @@
+from tsne_visual import TSNE
+import numpy as np
+import sys, os
+import pickle
+from sklearn.decomposition import PCA
+
+i_param = int(float(sys.argv[1].split('=')[1])) # specified through the command line !
+
+root_in= '/projectnb/fheating/SAT_GLASS/XORSAT/data/sol/' # root absolute path insert here ...
+root_out = '/projectnb/fheating/SAT_GLASS/XORSAT/analysis/TSNE2/'
+file_list = '/projectnb/fheating/SAT_GLASS/XORSAT/data/sol/file_name.txt' # absolute path insert here .. 
+
+i_count = 0
+fname_out = None
+for f in open(file_list,'r'):
+    if i_count == i_param:
+        fname_in = root_in+f.strip('\n')
+        sp = f.strip('\n').split('_')
+        param = sp[3:]
+        fname_out = root_out+'tSNE_'+'_'.join(sp[3:])
+        break
+    i_count+=1
+
+print('Reading from %s'%fname_in)
+print('Saving in %s'%fname_out)
+
+# ---------> RUNNING TSNE
+if fname_out is not None:
+    X = np.unpackbits(pickle.load(open(fname_in,'rb'))).astype(int).reshape(10000,-1) 
+    X = np.unique(X, axis=0)
+    model = TSNE(n_components=2, n_iter=2000, perplexity=50)
+    pca = PCA(n_components=100)
+    Xpca = pca.fit_transform(X)
+    Xtsne = model.fit_transform(Xpca)
+    pickle.dump([Xtsne, np.sum(pca.explained_variance_ratio_),len(X)], open(fname_out,'wb'))
--- XOR/analysis/plot_TSNE.py
+++ XOR/analysis/plot_TSNE.py
@@ -6,19 +6,23 @@ from sklearn.preprocessing import StandardScaler as SS
 
 N=4000
 K=3
-alpha_range = np.arange(0.5,1.001,0.05)
+alpha_range = np.arange(0.75,1.001,0.01)
 
-root= '/Users/alexandreday/GitProject/kSAT_sample/XOR/data/TSNE2/'
+root= '/Users/alexandreday/GitProject/kSAT_sample/XOR/data/TSNE3/'
 root_out= '/Users/alexandreday/GitProject/kSAT_sample/XOR/analysis/plots/'
+
 for a in alpha_range:
     print(a)
     f = 'tSNE_N=%i_a=%.3f_K=%i.pkl'%(N,a,K)
-    X, pca_r = pickle.load(open(root+f,'rb'))
-    X = SS().fit_transform(X)
-    modelfdc=FDC(eta = 0.0)
-    modelfdc.fit(X)
+    X, pca_r, lX = pickle.load(open(root+f,'rb'))
+    print(a, lX, pca_r,sep='\t')
 
-    plotting.cluster_w_label(X, modelfdc.cluster_label)
+    plt.scatter(X[:,0], X[:,1],s=0.5)
+    plt.show()
+    #X = SS().fit_transform(X)
+    #modelfdc=FDC(eta = 0.0)
+    #modelfdc.fit(X)
+    #plotting.cluster_w_label(X, modelfdc.cluster_label)
 
     """ plt.scatter(X[:,0], X[:,1], s=3, alpha=0.5)
     plt.title('$N=%i,\\alpha=%.3f, K=%i$'%(N,a,K))
